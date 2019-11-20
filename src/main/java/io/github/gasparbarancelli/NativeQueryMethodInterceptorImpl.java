f2519032f07c94cc81e1a1e8656818d3d9277d46
gaspar@viasoft.com.br <g45p4r19121987>
2019-11-20 14:48:34 -0300

    Uses NamedParameterJdbcTemplate to execute non-paging queries

--- src/main/java/io/github/gasparbarancelli/NativeQueryMethodInterceptorImpl.java
+++ src/main/java/io/github/gasparbarancelli/NativeQueryMethodInterceptorImpl.java
@@ -5,56 +5,88 @@ import org.hibernate.query.NativeQuery;
 import org.hibernate.transform.Transformers;
 import org.hibernate.type.LongType;
 import org.springframework.data.domain.PageImpl;
-
-import java.util.List;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import javax.persistence.EntityManager;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class NativeQueryMethodInterceptorImpl implements NativeQueryMethodInterceptor {
 
     @Override
     public Object executeQuery(NativeQueryInfo info) {
-        EntityManager entityManager = ApplicationContextProvider.getApplicationContext().getBean(EntityManager.class);
-        Session session = entityManager.unwrap(Session.class);
-        NativeQuery<?> query;
-        if (info.isEntity()) {
-            query = session.createNativeQuery(info.getSql(), info.getAliasToBean());
+        if (info.hasPagination() || info.isEntity()) {
+            EntityManager entityManager = ApplicationContextProvider.getApplicationContext().getBean(EntityManager.class);
+            Session session = entityManager.unwrap(Session.class);
+            NativeQuery<?> query;
+            if (info.isEntity()) {
+                query = session.createNativeQuery(info.getSql(), info.getAliasToBean());
+            } else {
+                query = session.createNativeQuery(info.getSql());
+            }
+
+            addParameterJpa(query, info);
+
+            if (info.hasPagination()) {
+                query.setFirstResult(info.getFirstResult());
+                query.setMaxResults(info.getMaxResult());
+            }
+            if (!info.isJavaObject() && !info.isEntity()) {
+                query.setResultTransformer(Transformers.aliasToBean(info.getAliasToBean()));
+            }
+            if (info.getReturnType().getSimpleName().equals(Void.TYPE.getName())) {
+                query.executeUpdate();
+                return null;
+            }
+            if (info.isSingleResult()) {
+                return query.getSingleResult();
+            }
+
+            List<?> resultList = query.list();
+            if (info.isPagination()) {
+                return new PageImpl(resultList, info.getPageable(), getTotalRecords(info, session));
+            }
+            return resultList;
         } else {
-            query = session.createNativeQuery(info.getSql());
-        }
+            NamedParameterJdbcTemplate jdbcTemplate = ApplicationContextProvider.getApplicationContext().getBean(NamedParameterJdbcTemplate.class);
 
-        addParameter(query, info);
+            Map<String, Object> parametroList = new HashMap<>();
+            for (NativeQueryParameter parametro : info.getParameterList()) {
+                if (parametro.getValue() != null && info.getSql().contains(":" + parametro.getName())) {
+                    parametroList.put(parametro.getName(), parametro.getValue());
+                }
+            }
 
-        if (info.hasPagination()) {
-            query.setFirstResult(info.getFirstResult());
-            query.setMaxResults(info.getMaxResult());
-        }
-        if (!info.isJavaObject() && !info.isEntity()) {
-            query.setResultTransformer(Transformers.aliasToBean(info.getAliasToBean()));
-        }
-        if (info.getReturnType().getSimpleName().equals(Void.TYPE.getName())) {
-            query.executeUpdate();
-            return null;
-        }
-        if (info.isSingleResult()) {
-            return query.getSingleResult();
-        }
+            BeanPropertyRowMapper<?> beanPropertyRowMapper = new BeanPropertyRowMapper<>(info.getAliasToBean());
+            if (info.getReturnType().getSimpleName().equals(Void.TYPE.getName())) {
+                jdbcTemplate.update(info.getSql(), parametroList);
+                return null;
+            }
 
-        List<?> resultList = query.list();
-        if (info.isPagination()) {
-            return new PageImpl(resultList, info.getPageable(), getTotalRecords(info, session));
+            if (info.isSingleResult()) {
+                if (info.isJavaObject()) {
+                    return jdbcTemplate.queryForObject(info.getSql(), parametroList, info.getAliasToBean());
+                }
+                return jdbcTemplate.queryForObject(info.getSql(), parametroList, beanPropertyRowMapper);
+            }
+
+            if (info.isJavaObject()) {
+                return jdbcTemplate.queryForList(info.getSql(), parametroList, info.getAliasToBean());
+            }
+            return jdbcTemplate.query(info.getSql(), parametroList, beanPropertyRowMapper);
         }
-        return resultList;
     }
 
     private Long getTotalRecords(NativeQueryInfo info, Session session) {
         NativeQuery<?> query = session.createNativeQuery(info.getSqlTotalRecord());
         query.unwrap(NativeQuery.class).addScalar("totalRecords", LongType.INSTANCE);
-        addParameter(query, info);
+        addParameterJpa(query, info);
         return (Long) query.getSingleResult();
     }
 
-    private void addParameter(NativeQuery<?> query, NativeQueryInfo info) {
+    private void addParameterJpa(NativeQuery<?> query, NativeQueryInfo info) {
         info.getParameterList().forEach(p -> {
             if (p.getValue() != null && info.getSql().contains(":" + p.getName())) {
                 query.setParameter(p.getName(), p.getValue());
