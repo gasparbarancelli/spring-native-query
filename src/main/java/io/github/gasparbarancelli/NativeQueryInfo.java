gasparbarancelli/spring-native-query

commit 8f2a5e3fdeac4bf0f9a5585dd5b162a3c2a7136d

Compile with Java 1.8.

src/main/java/io/github/gasparbarancelli/NativeQueryInfo.java
@@ -8,8 +8,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.util.ClassTypeInformation;
+import org.springframework.data.util.TypeInformation;
 
 import javax.persistence.Entity;
+
+import java.lang.reflect.Parameter;
 import java.util.*;
 
 @Getter
@@ -39,7 +42,7 @@ public class NativeQueryInfo {
     }
 
     public static NativeQueryInfo of(Class<? extends NativeQuery> classe, MethodInvocation invocation) {
-        var info = new NativeQueryInfo();
+        NativeQueryInfo info = new NativeQueryInfo();
 
         info.file = "nativeQuery/";
         if (classe.isAnnotationPresent(NativeQueryFolder.class)) {
@@ -63,13 +66,13 @@ public class NativeQueryInfo {
         info.parameterList = new ArrayList<>();
         info.pageable = null;
         for (int i = 0; i < invocation.getArguments().length; i++) {
-            var argument = invocation.getArguments()[i];
-            var parameter = invocation.getMethod().getParameters()[i];
+            Object argument = invocation.getArguments()[i];
+            Parameter parameter = invocation.getMethod().getParameters()[i];
             if (parameter.getType().isAssignableFrom(Pageable.class)) {
                 info.pageable = (Pageable) argument;
             } else {
                 if (parameter.isAnnotationPresent(NativeQueryParam.class)) {
-                    var param = parameter.getAnnotation(NativeQueryParam.class);
+                    NativeQueryParam param = parameter.getAnnotation(NativeQueryParam.class);
                     if (param.addChildren()) {
                         info.parameterList.addAll(NativeQueryParameter.ofDeclaredMethods(param.value(), parameter.getType(), argument));
                     } else {
@@ -84,7 +87,7 @@ public class NativeQueryInfo {
         info.returnType = invocation.getMethod().getReturnType();
         info.returnTypeIsIterable = Iterable.class.isAssignableFrom(info.returnType);
         if (info.returnTypeIsIterable) {
-            var componentType = ClassTypeInformation.fromReturnTypeOf(invocation.getMethod()).getComponentType();
+            TypeInformation<?> componentType = ClassTypeInformation.fromReturnTypeOf(invocation.getMethod()).getComponentType();
             info.aliasToBean = componentType.getType();
         } else {
             info.aliasToBean = info.returnType;
@@ -142,7 +145,7 @@ public class NativeQueryInfo {
     }
 
     boolean isJavaObject() {
-        return aliasToBean.getPackageName().startsWith("java");
+        return aliasToBean.getPackage().getName().startsWith("java");
     }
 
     boolean isPagination() {

pom.xml
@@ -26,9 +26,9 @@
     <properties>
         <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
         <project.reporting.outputEncoding>UTF-8</project.reporting.outputEncoding>
-        <maven.compiler.source>12</maven.compiler.source>
-        <maven.compiler.target>12</maven.compiler.target>
-        <java.version>12</java.version>
+        <maven.compiler.source>1.8</maven.compiler.source>
+        <maven.compiler.target>1.8</maven.compiler.target>
+        <java.version>1.8</java.version>
         <maven.gpg.plugin.version>1.6</maven.gpg.plugin.version>
         <maven.release.plugin.version>2.5.3</maven.release.plugin.version>
         <maven.scm.provider.gitexe.version>1.9.5</maven.scm.provider.gitexe.version>
@@ -141,10 +141,6 @@
                 <groupId>org.apache.maven.plugins</groupId>
                 <artifactId>maven-compiler-plugin</artifactId>
                 <version>3.8.1</version>
-                <configuration>
-                    <source>12</source>
-                    <target>12</target>
-                </configuration>
             </plugin>
             <plugin>
                 <artifactId>maven-deploy-plugin</artifactId>

src/main/java/io/github/gasparbarancelli/NativeQueryMethodInterceptorImpl.java
@@ -6,14 +6,16 @@ import org.hibernate.transform.Transformers;
 import org.hibernate.type.LongType;
 import org.springframework.data.domain.PageImpl;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 public class NativeQueryMethodInterceptorImpl implements NativeQueryMethodInterceptor {
 
     @Override
     public Object executeQuery(NativeQueryInfo info) {
-        var entityManager = ApplicationContextProvider.getApplicationContext().getBean(EntityManager.class);
-        var session = entityManager.unwrap(Session.class);
+        EntityManager entityManager = ApplicationContextProvider.getApplicationContext().getBean(EntityManager.class);
+        Session session = entityManager.unwrap(Session.class);
         NativeQuery query;
         if (info.isEntity()) {
             query = session.createNativeQuery(info.getSql(), info.getAliasToBean());
@@ -38,7 +40,7 @@ public class NativeQueryMethodInterceptorImpl implements NativeQueryMethodInterc
             return query.getSingleResult();
         }
 
-        var resultList = query.list();
+        List<?> resultList = query.list();
         if (info.isPagination()) {
             return new PageImpl(resultList, info.getPageable(), getTotalRecords(info, session));
         }

src/main/java/io/github/gasparbarancelli/NativeQueryParameter.java
@@ -20,7 +20,7 @@ class NativeQueryParameter {
     private Object value;
 
     static List<NativeQueryParameter> ofDeclaredMethods(String parentName, Class classe, Object object) {
-        var parameterList = new ArrayList<NativeQueryParameter>();
+        ArrayList<NativeQueryParameter> parameterList = new ArrayList<NativeQueryParameter>();
 
         @AllArgsConstructor
         class FieldInfo {
@@ -48,9 +48,9 @@ class NativeQueryParameter {
                 } catch (Exception ignore) {
                 }
 
-                var methodName = method.getName().substring(method.getName().startsWith("get") ? 3 : 2);
+                String methodName = method.getName().substring(method.getName().startsWith("get") ? 3 : 2);
 
-                var fieldInfo = mapField.get(methodName);
+                FieldInfo fieldInfo = mapField.get(methodName);
                 // todo implement ready method wihout field
                 if (fieldInfo != null) {
                     NativeQueryParam queryParam;
@@ -62,15 +62,15 @@ class NativeQueryParameter {
 
                     if (queryParam != null) {
                         if (queryParam.addChildren()) {
-                            var parentNameChildren = parentName + WordUtils.capitalize(queryParam.value());
+                            String parentNameChildren = parentName + WordUtils.capitalize(queryParam.value());
                             parameterList.addAll(ofDeclaredMethods(parentNameChildren, fieldInfo.type, value));
                         } else {
-                            var paramName = parentName + WordUtils.capitalize(queryParam.value());
-                            var paramValue = queryParam.operator().getTransformParam().apply(value);
+                            String paramName = parentName + WordUtils.capitalize(queryParam.value());
+                            Object paramValue = queryParam.operator().getTransformParam().apply(value);
                             parameterList.add(new NativeQueryParameter(paramName, paramValue));
                         }
                     } else {
-                        var paramValue = NativeQueryOperator.DEFAULT.getTransformParam().apply(value);
+                        Object paramValue = NativeQueryOperator.DEFAULT.getTransformParam().apply(value);
                         parameterList.add(new NativeQueryParameter(parentName + methodName, paramValue));
                     }
                 }

src/main/java/io/github/gasparbarancelli/NativeQueryProxyFactoryImpl.java
@@ -21,7 +21,7 @@ public class NativeQueryProxyFactoryImpl implements NativeQueryProxyFactory {
             if ("toString".equals(invocation.getMethod().getName())) {
                 return "NativeQuery Implementation";
             }
-            var info = NativeQueryInfo.of(classe, invocation);
+            NativeQueryInfo info = NativeQueryInfo.of(classe, invocation);
             return nativeQueryMethodInterceptor.executeQuery(info);
         });
         return proxy.getProxy(classe.getClassLoader());

src/main/java/io/github/gasparbarancelli/NativeQueryRegistryImpl.java
@@ -1,5 +1,6 @@
 package io.github.gasparbarancelli;
 
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 
 import java.beans.Introspector;
@@ -19,9 +20,9 @@ public class NativeQueryRegistryImpl implements NativeQueryRegistry {
     @Override
     public void registry(Set<Class<? extends NativeQuery>> nimitzNativeQueryList) {
         for (Class<? extends NativeQuery> classe : nimitzNativeQueryList) {
-            var source = nativeQueryProxyFactory.create(classe);
-            var beanDefinition = NativeQueryBeanDefinition.of(classe, source);
-            var beanName = Introspector.decapitalize(classe.getSimpleName());
+            Object source = nativeQueryProxyFactory.create(classe);
+            AbstractBeanDefinition beanDefinition = NativeQueryBeanDefinition.of(classe, source);
+            String beanName = Introspector.decapitalize(classe.getSimpleName());
             registry.registerBeanDefinition(beanName, beanDefinition);
         }
     }

commit 5ea47329be06b325543a53d21f46016fad5aafbe

Fix NPE in isJavaObject. This issue was introduced in 8f2a5e3fdeac4bf0f9a5585dd5b162a3c2a7136d.

src/main/java/io/github/gasparbarancelli/NativeQueryInfo.java
@@ -162,7 +162,22 @@ public class NativeQueryInfo {
     }
 
     boolean isJavaObject() {
-        return aliasToBean.getPackage().getName().startsWith("java");
+        return getPackageName(aliasToBean).startsWith("java");
+    }
+
+    private String getPackageName(Class<?> c) {
+        final String pn;
+        while (c.isArray()) {
+            c = c.getComponentType();
+        }
+        if (c.isPrimitive()) {
+            pn = "java.lang";
+        } else {
+            String cn = c.getName();
+            int dot = cn.lastIndexOf('.');
+            pn = (dot != -1) ? cn.substring(0, dot).intern() : "";
+        }
+        return pn;
     }
 
     boolean isPagination() {